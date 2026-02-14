b4084f9dd295d6fc4e5ac5474a3de5cd0142c513
Anwar Hussaini <anwarhussaini160@gmail.com>
2026-02-14 23:32:44 +0430

    Add TextReverseView implementation with form validation and UI layout

---
+++ lib/ui/views/text_reverse/text_reverse_view.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stacked/ui/common/ui_helpers.dart';
+import 'package:flutter_stacked/ui/views/text_reverse/text_reverse_view.form.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked/stacked_annotations.dart';
+
+import 'text_reverse_viewmodel.dart';
+
+@FormView(
+  fields: [
+    FormTextField(
+      name: 'name',
+      validator: TextReverseValidators.validateReverseText,
+    ),
+  ],
+)
+class TextReverseView extends StackedView<TextReverseViewModel>
+    with $TextReverseView {
+  const TextReverseView({Key? key}) : super(key: key);
+
+  @override
+  Widget builder(
+    BuildContext context,
+    TextReverseViewModel viewModel,
+    Widget? child,
+  ) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Text Reverser')),
+      body: Container(
+        padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              verticalSpaceMedium,
+              const Text(
+                'Text to Reverse',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+              ),
+              verticalSpaceSmall,
+              TextFormField(controller: nameController),
+              if (viewModel.hasNameValidationMessage) ...[
+                verticalSpaceTiny,
+                Text(
+                  viewModel.nameValidationMessage!,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+              verticalSpaceMedium,
+              Text(
+                viewModel.reversedText,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  TextReverseViewModel viewModelBuilder(BuildContext context) {
+    return TextReverseViewModel();
+  }
+
+  @override
+  void onViewModelReady(TextReverseViewModel viewModel) {
+    syncFormWithViewModel(viewModel);
+  }
+
+  @override
+  void onDispose(TextReverseViewModel viewModel) {
+    disposeForm();
+    super.onDispose(viewModel);
+  }
+}
