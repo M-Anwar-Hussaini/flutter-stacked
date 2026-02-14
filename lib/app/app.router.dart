a9a3157f95a775ce2dc0efdb6c78d378ccd24b72
Anwar Hussaini <anwarhussaini160@gmail.com>
2026-02-14 23:32:31 +0430

    Update app.router.dart to include TextReverseView in routing and navigation methods

--- lib/app/app.router.dart
+++ lib/app/app.router.dart
@@ -5,14 +5,16 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:flutter/material.dart' as _i6;
+import 'package:flutter/material.dart' as _i7;
 import 'package:flutter/material.dart';
 import 'package:flutter_stacked/ui/views/counter/counter_view.dart' as _i4;
 import 'package:flutter_stacked/ui/views/home/home_view.dart' as _i2;
 import 'package:flutter_stacked/ui/views/login/login_view.dart' as _i5;
 import 'package:flutter_stacked/ui/views/startup/startup_view.dart' as _i3;
+import 'package:flutter_stacked/ui/views/text_reverse/text_reverse_view.dart'
+    as _i6;
 import 'package:stacked/stacked.dart' as _i1;
-import 'package:stacked_services/stacked_services.dart' as _i7;
+import 'package:stacked_services/stacked_services.dart' as _i8;
 
 class Routes {
   static const homeView = '/home-view';
@@ -23,7 +25,15 @@ class Routes {
 
   static const loginView = '/login-view';
 
-  static const all = <String>{homeView, startupView, counterView, loginView};
+  static const textReverseView = '/text-reverse-view';
+
+  static const all = <String>{
+    homeView,
+    startupView,
+    counterView,
+    loginView,
+    textReverseView,
+  };
 }
 
 class StackedRouter extends _i1.RouterBase {
@@ -32,32 +42,39 @@ class StackedRouter extends _i1.RouterBase {
     _i1.RouteDef(Routes.startupView, page: _i3.StartupView),
     _i1.RouteDef(Routes.counterView, page: _i4.CounterView),
     _i1.RouteDef(Routes.loginView, page: _i5.LoginView),
+    _i1.RouteDef(Routes.textReverseView, page: _i6.TextReverseView),
   ];
 
   final _pagesMap = <Type, _i1.StackedRouteFactory>{
     _i2.HomeView: (data) {
       final args = data.getArgs<HomeViewArguments>(nullOk: false);
-      return _i6.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) =>
             _i2.HomeView(key: args.key, firstIndex: args.firstIndex),
         settings: data,
       );
     },
     _i3.StartupView: (data) {
-      return _i6.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i3.StartupView(),
         settings: data,
       );
     },
     _i4.CounterView: (data) {
-      return _i6.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i4.CounterView(),
         settings: data,
       );
     },
     _i5.LoginView: (data) {
-      return _i6.MaterialPageRoute<dynamic>(
+      return _i7.MaterialPageRoute<dynamic>(
         builder: (context) => const _i5.LoginView(),
+        settings: data,
+      );
+    },
+    _i6.TextReverseView: (data) {
+      return _i7.MaterialPageRoute<dynamic>(
+        builder: (context) => const _i6.TextReverseView(),
         settings: data,
       );
     },
@@ -73,7 +90,7 @@ class StackedRouter extends _i1.RouterBase {
 class HomeViewArguments {
   const HomeViewArguments({this.key, required this.firstIndex});
 
-  final _i6.Key? key;
+  final _i7.Key? key;
 
   final int firstIndex;
 
@@ -94,9 +111,9 @@ class HomeViewArguments {
   }
 }
 
-extension NavigatorStateExtension on _i7.NavigationService {
+extension NavigatorStateExtension on _i8.NavigationService {
   Future<dynamic> navigateToHomeView({
-    _i6.Key? key,
+    _i7.Key? key,
     required int firstIndex,
     int? routerId,
     bool preventDuplicates = true,
@@ -162,8 +179,24 @@ extension NavigatorStateExtension on _i7.NavigationService {
     );
   }
 
+  Future<dynamic> navigateToTextReverseView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+    transition,
+  ]) async {
+    return navigateTo<dynamic>(
+      Routes.textReverseView,
+      id: routerId,
+      preventDuplicates: preventDuplicates,
+      parameters: parameters,
+      transition: transition,
+    );
+  }
+
   Future<dynamic> replaceWithHomeView({
-    _i6.Key? key,
+    _i7.Key? key,
     required int firstIndex,
     int? routerId,
     bool preventDuplicates = true,
@@ -222,6 +255,22 @@ extension NavigatorStateExtension on _i7.NavigationService {
   ]) async {
     return replaceWith<dynamic>(
       Routes.loginView,
+      id: routerId,
+      preventDuplicates: preventDuplicates,
+      parameters: parameters,
+      transition: transition,
+    );
+  }
+
+  Future<dynamic> replaceWithTextReverseView([
+    int? routerId,
+    bool preventDuplicates = true,
+    Map<String, String>? parameters,
+    Widget Function(BuildContext, Animation<double>, Animation<double>, Widget)?
+    transition,
+  ]) async {
+    return replaceWith<dynamic>(
+      Routes.textReverseView,
       id: routerId,
       preventDuplicates: preventDuplicates,
       parameters: parameters,
